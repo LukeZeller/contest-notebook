fae60a9c56248e4e6271421a0712b4410e4664dc
s-nandi <saujas.nandi@gmail.com>
2018-03-29 01:31:50 -0400

    Create prerequisites.cpp

---
+++ computational-geometry/polygon/convex-polygon/prerequisites.cpp
@@ -0,0 +1,38 @@
+/*
+* Description: convex polygon base code
+*/
+
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define ll long long
+
+typedef ll ptT; //PS: otherwise long double
+
+const double EPS = 1e-9;
+
+struct pt
+{
+    ptT x, y;
+
+    bool operator < (const pt &o) const {return make_pair(y, x) < make_pair(o.y, o.x);}
+    bool operator == (const pt &o) const {return abs(x - o.x) < EPS and abs(y - o.y) < EPS;}
+
+    pt operator - (const pt &o) const {return {x - o.x, y - o.y};}
+
+    ptT operator ^ (const pt &o) const {return x * o.y - y * o.x;}
+    ptT operator * (const pt &o) const {return x * o.x + y * o.y;}
+
+    ptT norm2() const {return *this * *this;}
+    ptT dist2(const pt &o) const {return (*this - o).norm2();}
+};
+
+int sgn(ptT i) {return (i > -EPS) - (i < EPS);}
+int orientation(pt &o, pt &a, pt &b) {return sgn((b - o) ^ (a - o));} //cw: 1, ccw: -1, col: 0
+
+typedef vector<pt> polygon;
+int prev(int i, int n, int st = 1){return (i - st + n) % n;}
+int next(int i, int n, int st = 1){return (i + st) % n;};
