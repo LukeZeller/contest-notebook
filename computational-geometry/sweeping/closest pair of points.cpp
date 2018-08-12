92e7ecaea8d205688605cc4cbead0f5eef3de0af
s-nandi <saujas.nandi@gmail.com>
2018-08-12 19:20:21 -0400

    Update closest pair of points.cpp

--- computational-geometry/sweeping/closest pair of points.cpp
+++ computational-geometry/sweeping/closest pair of points.cpp
@@ -1,38 +1,10 @@
 /*
 * Description: closest pair of points (via line sweep)
 * Demo: given vector of points returns pair of closest points
+* Dependencies: pt class
 */
 
-#include <iostream>
-#include <vector>
-#include <algorithm>
-#include <set>
-#include <cmath>
-
-using namespace std;
-
-#define ll long long
-
-typedef int ptsT; //PS: otherwise double
-typedef ll ptlT; //PS: otherwise long double
-
-const ll INF = 1e18;
-
-struct pt
-{
-    ptsT x, y;
-
-    bool operator < (const pt &o) const {return make_pair(y, x) < make_pair(o.y, o.x);}
-
-    pt operator - (const pt &o) const {return {x - o.x, y - o.y};}
-
-    ptlT operator * (const pt &o) const {return (ptlT) x * o.x + (ptlT) y * o.y;}
-
-    ptlT norm2() const {return *this * *this;}
-    ptlT dist2(const pt &o) const {return (*this - o).norm2();}
-};
-
-inline ptlT square(ptsT i) {return (ptlT) i * i;}
+inline ptT square(ptT i) {return i * i;}
 
 pair <pt, pt> closestPairPoints(vector <pt> &points)
 {
@@ -55,24 +27,4 @@ pair <pt, pt> closestPairPoints(vector <pt> &points)
         window.insert(points[i]);
     }
     return res;
-}
-
-int main()
-{
-    vector <pt> points = {{0, 0}, {-4, 1}, {-7, -2}, {4, 5}, {1, 1}};
-
-    auto sol = closestPairPoints(points);
-    pt a = sol.first, b = sol.second;
-
-    //Expected: 0 0 and 4 4 with distance: 1.414214
-    cout<<a.x<<" "<<a.y<<" and "<<b.x<<" "<<b.y<<" with distance: "<<sqrt(a.dist2(b))<<'\n';
-
-    vector <pt> points2 = {{0, 0}, {100, 45}, {2, 3}, {9, 9}, {0, 1}};
-    sol = closestPairPoints(points2);
-    a = sol.first, b = sol.second;
-
-    //Expected: 0 0 and 0 1 with distance: 1.0
-    cout<<a.x<<" "<<a.y<<" and "<<b.x<<" "<<b.y<<" with distance: "<<sqrt(a.dist2(b))<<'\n';
-
-    return 0;
 }
