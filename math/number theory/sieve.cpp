c4474bf12bc2d303a7c104987f9363107e5fa89d
s-nandi <saujas.nandi@gmail.com>
2018-08-17 04:24:58 -0400

    Update sieve.cpp

--- math/number theory/sieve.cpp
+++ math/number theory/sieve.cpp
@@ -3,20 +3,13 @@
 * Demo: isPrime() checks if n is prime, factorize() returns list of (factor, power) pairs
 */
 
-#include <iostream>
-#include <vector>
-#include <cmath>
-
-using namespace std;
-
 const int MAXN = 1000000000;
 
 vector <bool> prm;
 vector <int> primes;
-
-void sieve()
+void sieve(int n)
 {
-    int sq = sqrt(MAXN) + 100;
+    int sq = sqrt(n) + 100;
     prm.resize(sq, true);
     for (int i = 2; i < sq; i++)
     {
@@ -32,7 +25,8 @@ bool isPrime(int n)
     if (n < prm.size()) return prm[n];
     for (int p: primes)
     {
-        if (n % p == 0) return false;
+        if (n % p == 0) 
+            return false;
     }
     return true;
 }
@@ -52,7 +46,7 @@ vector <pair<int, int>> factorize(int n)
 
 int main()
 {
-    sieve();
+    sieve(MAXN);
 
     //Expected: 1 1 1 0 0
     cout<<isPrime(3)<<" "<<isPrime(1000000007)<<" "<<isPrime(37)<<" "<<isPrime(103793)<<" "<<isPrime(6)<<'\n';
