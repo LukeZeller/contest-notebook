a2363bd773e6eb85288bf749d6f1a68e7ed07ea6
s-nandi <saujas.nandi@gmail.com>
2018-08-23 22:30:53 -0400

    Update and rename gcd.cpp to gcd and lcm.cpp

--- math/number theory/gcd and lcm.cpp
+++ math/number theory/gcd and lcm.cpp
@@ -1,12 +1,16 @@
 /*
 *  Description: gcd via the euclidean algorithm
+*               lcm using following property: lcm(a, b) * gcd(a, b) = a * b
 */
 
 int gcd(int a, int b)
 {
     while (b != 0)
-    {
         tie(a, b) = make_tuple(b, a % b);
-    }
     return a;
+}
+
+int lcm(int a, int b)
+{
+    return a / gcd(a, b) * b;
 }
