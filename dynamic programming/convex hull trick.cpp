01facb9329f4d41eb5556e06de885abc33ad9935
s-nandi <saujas.nandi@gmail.com>
2018-09-26 15:38:53 -0400

    Update convex hull trick.cpp

--- dynamic programming/convex hull trick.cpp
+++ dynamic programming/convex hull trick.cpp
@@ -1,41 +1,53 @@
 /*
 * Description: convex hull trick for finding max/min
-* Demo: static cht (queries only increasing) to find min using set of linear functions
+* Demo: static cht (queries only increasing) and dynamic cht (arbitrary queries) to find min using set of linear functions at given x-value
 */
-
-#include <iostream>
-#include <vector>
-#include <deque>
-
-using namespace std;
-
-#define ll long long
 
 struct equation
 {
-    ll m, b;
+    ll a, b; // y = a * x + b
 
-    ll y(ll x0){return m * x0 + b;} //PS: different for quadtratic
-    double intersect(equation o){return double(o.b - b) / (m - o.m);} //PS: same as above
+    ll y(ll x0){return a * x0 + b;}
+    long double intersect(equation o){return (long double) (o.b - b) / (a - o.a);}
 };
 
+// Requires monotone decreasing slope
+// Use static getMin if monotone increasing x0, otherwise use dynamic getMin
+// negate y() in equation to get maximum (monotone increasing slope needed for static)
 struct convexHullTrick
 {
     deque <equation> hull;
 
-    void addEquation(const equation &e) //PS: negate values in equation to get maximum
+    void addEquation(const equation &e)
     {
         while (hull.size() > 1 and hull.rbegin()[1].intersect(e) < hull.rbegin()[1].intersect(hull.rbegin()[0]))
             hull.pop_back();
         hull.push_back(e);
     }
 
-    ll getValue(ll x0)
+    ll getMinDynamic(ll x0)
     {
-        if (hull.empty()) return 0;
+        if (hull.size() == 0)
+            return 0; // PS: return value in case no functions exist
+
+        int l = 0, r = hull.size() - 1;
+        while (l < r)
+        {
+            int m = (l + r) / 2;
+            if (hull[m].intersect(hull[m + 1]) >= x0)
+                r = m;
+            else
+                l = m + 1;
+        }
+        return hull[l].y(x0);
+    }
+
+    ll getMinStatic(ll x0)
+    {
+        if (hull.empty()) return 0; //PS: same as above
         while (hull.size() > 1 and hull[0].y(x0) > hull[1].y(x0))
             hull.pop_front();
-        return hull[0].y(x0); //PS: negate result if negated equations
+        return hull[0].y(x0);
     }
 };
 
@@ -43,9 +55,7 @@ int main()
 {
     convexHullTrick cht;
     cht.addEquation({3, 2}), cht.addEquation({1, 6}), cht.addEquation({0, 7});
-    //Expected: 2 5 7 7 7
-    for (int i = 0; i < 5; i++) cout<<cht.getValue(i)<<" ";
+    //Expected: (2 2) (5 5) (7 7) (7 7) (7 7)
+    for (int i = 0; i < 5; i++) cout<<"("<<cht.getMinStatic(i)<<" "<<cht.getMinDynamic(i)<<") ";
     cout<<'\n';
-
-    return 0;
 }
