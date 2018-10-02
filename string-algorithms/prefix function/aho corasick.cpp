a9f5ab58160f71da635b77dfdf45f880fae5cc94
s-nandi <saujas.nandi@gmail.com>
2018-10-02 16:29:49 -0400

    Update aho corasick.cpp

--- string-algorithms/prefix function/aho corasick.cpp
+++ string-algorithms/prefix function/aho corasick.cpp
@@ -1,128 +1,119 @@
 /*
 * Description: aho-corasick automata for matching text queries with pattern set
-* Demo: printAllMatches() outputs all pattern instances (with repetition) and returns first instance of a pattern
+* Demo: nextState takes state and transition and returns resulting state, isTerminal returns true iff the state corresponds to the end of a string (can be memoized)
+        getStr() prints the string associated with a state, printAllMatches prints all strings in automata that are contained in input string
 */
 
-#include <iostream>
-#include <vector>
-#include <map>
-#include <queue>
-
-using namespace std;
-
-const int MAXN = 100001; /*PS*/
-
+const int alpha = 256; /*PS*/
 struct node
 {
-    map <int, int> to;
-    int prefixLink = -1, matchLink = -1, len = 0;
-    bool isEnd = false;
+    map <int, int> next; // PS: next/go can be any combination of map/array
+    array <int, alpha> go;
+    int terminalIdx = -1, prefixLink = -1, matchLink = -1; // iff terminalIdx != -1, it denotes the index of the string ending at this node
+    int par, pch;
+
+    node(int p = -1, int ch = -1) : par(p), pch(ch) {fill(go.begin(), go.end(), -1);} /*PS*/
 };
 
 struct ahoCorasick
 {
-    int numNodes;
+    int sz;
     vector <node> elements;
 
-    ahoCorasick()
-    {
-        numNodes = 1;
-        elements.resize(MAXN);
-    }
+    ahoCorasick(){sz = 1, elements.resize(1);}
 
-    int mapping(char c){return c + 128;} /*PS*/
+    int mapping(unsigned char c){return c;}
+    unsigned char invmapping(int i){return i;}
 
-    void addWord(string &s)
+    int next(int v, int t){return elements[v].next.count(t) ? elements[v].next[t] : -1;}
+    int go(int v, int t){return elements[v].go[t];}
+
+    void addWord(string const &s, int idx = -2)
     {
-        int curr = 0;
-        for (char c: s)
+        int v = 0;
+        for (char ch : s)
         {
-            int mc = mapping(c);
-            if (next(curr, mc) == -1)
+            int c = mapping(ch);
+            if (next(v, c) == -1)
             {
-                elements[curr].to[mc] = numNodes++;
-                elements[next(curr, mc)].len = elements[curr].len + 1;
+                elements[v].next[c] = sz++;
+                elements.emplace_back(v, c);
             }
-            curr = next(curr, mc);
+            v = next(v, c);
         }
-        elements[curr].isEnd = true;
+        elements[v].terminalIdx = idx;
     }
 
-    int next(int i, int t){return elements[i].to.count(t) ? elements[i].to[t] : -1;}
-
-    void build()
+    int getLink(int v)
     {
-        elements[0].prefixLink = 0;
-        buildLinks();
-    }
-
-    void buildLinks()
-    {
-        queue <int> q;
-        q.push(0);
-
-        while (!q.empty())
+        if (elements[v].prefixLink == -1)
         {
-            int curr = q.front();
-            q.pop();
+            elements[v].prefixLink = elements[v].par <= 0 ? 0 : nextState(getLink(elements[v].par), elements[v].pch);
+            getLink(elements[v].prefixLink);
 
-            int j = elements[curr].prefixLink;
-            elements[curr].matchLink = elements[j].isEnd ? j : elements[j].matchLink;
-            for (auto &elem: elements[curr].to)
-            {
-                int mc = elem.first;
-
-                while (j > 0 and next(j, mc) == -1) j = elements[j].prefixLink;
-                if (curr != 0 and next(j, mc) != -1) elements[next(curr, mc)].prefixLink = next(j, mc);
-                else elements[next(curr, mc)].prefixLink = 0;
-
-                q.push(next(curr, mc));
-            }
+            int link = elements[v].prefixLink;
+            elements[v].matchLink = (elements[link].terminalIdx != -1) ? link: elements[link].matchLink;
         }
+        return elements[v].prefixLink;
     }
 
-    const int INF = 1231231234; //PS: not needed if finding arbitrary/all match(es)
+    int nextState(int v, int c)
+    {
+        if (go(v, c) == -1)
+        {
+            if (next(v, c) != -1)
+                elements[v].go[c] = next(v, c);
+            else
+                elements[v].go[c] = v == 0 ? 0 : nextState(getLink(v), c);
+        }
+        return go(v, c);
+    }
+
+    bool isTerminal(int v)
+    {
+        getLink(v);
+        return elements[v].terminalIdx != -1 or (elements[v].matchLink != -1 and isTerminal(elements[v].matchLink));
+    }
+
+    string getStr(int v)
+    {
+        string res = "";
+        for (int j = v; elements[j].par != -1; j = elements[j].par)
+            res += invmapping(elements[j].pch);
+        reverse(res.begin(), res.end());
+        return res;
+    }
+
     int printAllMatches(const string &s)
     {
-        int first = INF;
-        for (int i = 0, j = 0; i < s.length(); i++)
+        int cnt = 0;
+        for (int i = 0, state = 0; i < s.length(); i++)
         {
-            int mc = mapping(s[i]);
-
-            while (j > 0 and next(j, mc) == -1) j = elements[j].prefixLink;
-            if (next(j, mc) != -1) j = next(j, mc);
-            else j = 0;
-
-            for (int k = j; k != -1; k = elements[k].matchLink) if (elements[k].isEnd)
-            {
-                cout<<s.substr(i - elements[k].len + 1, elements[k].len)<<" ";
-                first = min(first, i - elements[k].len + 1);
-            }
+            state = nextState(state, mapping(s[i]));
+            getLink(state);
+            for (int j = state; j != -1; j = elements[j].matchLink)
+                if (elements[j].terminalIdx != -1)
+                {
+                    cout << getStr(j) << " ";
+                    cnt++;
+                }
         }
-        return first;
+        return cnt;
     }
 };
 
 int main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-
     vector <string> patterns = {"hello", "world", "hi", "he", "orld"};
 
     ahoCorasick ac;
     for (string s: patterns) ac.addWord(s);
-    ac.build();
 
     //Expected: he hi he hello
     ac.printAllMatches("heyworlhihello");
     cout<<'\n';
 
-    //Expected: world orld
-    int first = ac.printAllMatches("sworld");
-    cout<<'\n';
-    //Expected: 1
-    cout<<first<<'\n';
-
-    return 0;
+    //Expected: world orld 2
+    int numMatch = ac.printAllMatches("sworld");
+    cout<<numMatch << '\n';
 }
