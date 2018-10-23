1f2c39bd40ffd1f3cf70a4c701497edfd6e6e7ea
s-nandi <saujas.nandi@gmail.com>
2018-10-23 19:56:47 -0400

    Update line.cpp

--- computational-geometry/line.cpp
+++ computational-geometry/line.cpp
@@ -46,11 +46,22 @@ struct line
     }
 };
 
-bool intersectionSegment(const line &m, const line &n)
+// returns true if [l1, r1] intersects [l2, r2] on number line
+bool intersect1d(ptT l1, ptT r1, ptT l2, ptT r2)
 {
-    if (orientation(m.a, m.b, n.a) == orientation(m.a, m.b, n.b)) return false;
-    if (orientation(n.a, n.b, m.a) == orientation(n.a, n.b, m.b)) return false;
-    return true;
+    if (l1 > r1)
+        swap(l1, r1);
+    if (l2 > r2)
+        swap(l2, r2);
+    return max(l1, l2) <= min(r1, r2) + EPS;
+}
+
+bool intersectionSegment(const line& m, const line& n)
+{
+    return intersect1d(m.a.x, m.b.x, n.a.x, n.b.x) and
+           intersect1d(m.a.y, m.b.y, n.a.y, n.b.y) and
+           orientation(m.a, m.b, n.a) * orientation(m.a, m.b, n.b) <= 0 and
+           orientation(n.a, n.b, m.a) * orientation(n.a, n.b, m.b) <= 0;
 }
 
 pair <bool, pt> findIntersection(const line &m, const line &n, ii mtype, ii ntype) // pairs represent left/right endpoints, 0: no endpoint, 1: open endpoint, 2: closed endpoint
