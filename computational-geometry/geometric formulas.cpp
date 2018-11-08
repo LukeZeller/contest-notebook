9cbab54642f82f9b87276a18ed7113db18225dee
s-nandi <saujas.nandi@gmail.com>
2018-11-07 19:33:32 -0500

    Create geometric formulas.cpp

---
+++ computational-geometry/geometric formulas.cpp
@@ -0,0 +1,11 @@
+/*
+* Description: Misc. geometric formulas/facts
+* Demo: Euler's characteristic, Pick's theorem
+*/
+
+Euler's characteristic for planar graphs/polyhedra: V + F = E + 2 
+  where V = |Vertices|, F = |Faces| (including exterior face), E = |Edges|
+  
+ 
+ Pick's Theorem for integer coordinate simple polygons: A = i + b / 2 + 1
+  where A = area of polygon, i = # of strictly interior points, b = # of points on boundary of polygon
