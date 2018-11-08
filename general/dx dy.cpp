a503ac0f9a31ef010e6fbcdec46009c9bcb25a4f
s-nandi <saujas.nandi@gmail.com>
2018-11-08 18:33:00 -0500

    Create dx dy.cpp

---
+++ general/dx dy.cpp
@@ -0,0 +1,16 @@
+/*
+* Description: dx/dy vectors
+* Demo: grid moves (NWES, 8 directional), knight moves
+*/
+
+Knight Moves:
+vector <int> dx = {-1, -2, -2, -1, 1, 2, 2, 1};
+vector <int> dy = {-2, -1, 1, 2, 2, 1, -1, -2};
+
+Grid Moves (4 dir):
+vector <int> dx = {0, -1, 0, 1};
+vector <int> dy = {-1, 0, 1, 0};
+
+Grid Moves (8 dir):
+vector <int> dx = {-1, -1, 0, 1, 1, 1, 0, -1};
+vector <int> dy = {0, -1, -1, -1, 0, 1, 1, 1};
